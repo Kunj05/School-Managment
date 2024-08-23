55b7870208cae669d5e2fbbda6bdadf97dd039e7
Kunj05 <kunjchandak05@gmail.com>
2024-08-24 01:26:36 +0530

    Completed both the endpoints

--- config/db.js
+++ config/db.js
@@ -2,20 +2,24 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: parseInt(process.env.MYSQLPORT, 10),
+  port: process.env.MYSQL_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+
 });
 
-// console.log('Database connection pool created with the following configuration:');
-// console.log(`Host: ${process.env.MYSQLHOST}`);
-// console.log(`User: ${process.env.MYSQLUSER}`);
-// console.log(`Database: ${process.env.MYSQL_DATABASE}`);
-// console.log(`Port: ${process.env.MYSQLPORT}`);
+pool.getConnection()
+  .then(connection => {
+    console.log('Connected to MySQL database successfully.');
+    connection.release();
+  })
+  .catch(err => {
+    console.error('Error connecting to MySQL database:', err.message);
+  });
 
 module.exports = pool;
